d2ce2f9922b224fcfb7285757f4defeb2f7ee982
BBlue530 <davidlundegard@gmail.com>
2025-04-12 23:53:49 +0200

    CreateKey added

--- AWS_Server/AWS_Server.py
+++ AWS_Server/AWS_Server.py
@@ -1,6 +1,7 @@
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import boto3
+import uuid
 
 # I WILL APPEAR IF EVERYTHING WORKED LIKE IT SHOULD
 
@@ -44,6 +45,40 @@ def check_key(event):
             "statusCode": 500,
             "body": json.dumps({"message": "Internal error", "error": str(e)})
         }
+    
+def create_key(event):
+    dynamodb = boto3.resource("dynamodb", region_name="eu-north-1", endpoint_url="https://dynamodb.eu-north-1.amazonaws.com")
+    table = dynamodb.Table("Key_Validation")
+    expiration_days=30
+
+    try:
+        client_name = event["body"]["name"]
+    except KeyError as e:
+        print(f"Error: {str(e)}")
+        return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Invalid input"})
+          }
+
+    license_key = str(uuid.uuid4())
+
+    # Expiration date
+    expiration_date = (datetime.now() + timedelta(days=expiration_days)).strftime("%Y-%m-%d")
+
+    # Store key DynamoDB
+    table.put_item(
+    Item={
+        "LicenseKey": license_key,
+        "ClientName": client_name,
+        "ExpirationDate": expiration_date,
+    }
+)
+
+    return {
+                "statusCode": 200,
+                "body": json.dumps({"message": f"License Key Generated: {client_name}: {license_key} Expire: {expiration_date})"})
+          }
+    
 
 def lambda_handler(event, context):
     print(json.dumps(event))
@@ -59,3 +94,22 @@ def lambda_handler(event, context):
     if endpoint == "/v1/CheckKey":
         response = check_key(event)
         return response
+    elif endpoint == "/v1/CreateKey":
+        try:
+            api_key = event["headers"]["key"]
+        except KeyError as e:
+            print(f"Error: {str(e)}")
+            return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "Invalid input"})
+        }
+
+        if api_key == "12345":
+            response = create_key(event)
+            return response
+        else:
+            print("Invalid Key")
+            return {
+            "statusCode": 400,
+            "body": json.dumps({"message": "Invalid input"})
+        }
